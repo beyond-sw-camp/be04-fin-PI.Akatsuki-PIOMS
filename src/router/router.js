36ffef1a31d78bfb4a3578b2f87a90ec5fd52cca
악덕 <rudejrqkr97@gmail.com>
2024-05-22 16:57:45 +0900

    feat: Administrator login processing completed
    - 로그인 페이지에 'DriverLogin', 'FranchiseLogin', 'CommonLogin' 컴포넌트 추가
    - Vuex 스토어에 인증 정보를 관리하기 위한 상태 추가
    - 각 로그인 컴포넌트에 로그인 API 호출 및 로직 추가
    - 라우터에 새로운 로그인 페이지 경로 및 컴포넌트 등록.

--- src/router/router.js
+++ src/router/router.js
@@ -2,8 +2,32 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AskMain from "@/components/franchise/ask/AskMain.vue";
 import AnswerFormEdit from "@/components/franchise/ask/AnswerFormEdit.vue";
 import AnswerFormRegister from "@/components/franchise/ask/AnswerFormRegister.vue";
+import AdminLogin from "@/components/login/AdminLogin.vue";
+import FranchiseLogin from "@/components/login/FranchiseLogin.vue";
+import DriverLogin from "@/components/login/DriverLogin.vue";
+import CommonLogin from "@/components/login/CommonLogin.vue";
 
 const routes = [
+    {
+        path: '/admin/login',
+        name: 'AdminLogin',
+        component: AdminLogin,
+    },
+    {
+        path: '/franchise/login',
+        name: 'FranchiseLogin',
+        component: FranchiseLogin,
+    },
+    {
+        path: '/driver/login',
+        name: 'DriverLogin',
+        component: DriverLogin,
+    },
+    {
+        path: '/login',
+        name: 'CommonLogin',
+        component: CommonLogin,
+    },
     {
         path: '/',
         name: 'AskMain',
