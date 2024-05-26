b15d8cd8345ad1281d5fa33b108d87b96c3b9c17
악덕 <rudejrqkr97@gmail.com>
2024-05-27 04:28:15 +0900

    design: #41 Login selection screen implementation
    - 로그인 선택창 구현

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,3 @@
 <template>
-<!--  <router-view></router-view>-->
-
-    <main>
-<!--      <Breadcrumb :crumbs="breadcrumbs" />-->
-<!--      <AskMain/>-->
-<!--<CategoryList/>-->
-  <ProductList/>
-    </main>
+  <router-view></router-view>
 </template>
-
-<script setup>
-// import AskMain from "@/components/amdin/ask/AskMain.vue";
-// import Breadcrumb from "@/components/amdin/ask/Breadcrumb.vue";
-// import CategoryList from "@/components/amdin/Category/CategoryList.vue";
-import ProductList from "@/components/amdin/product/ProductList.vue";
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
