234597b13290a07f7ba32aa557ca9b9d00100cc9
악덕 <rudejrqkr97@gmail.com>
2024-05-27 13:59:24 +0900

    ci/cd: modify deploy.yml file
    - deploy.yml 파일 수정
    - .ebextensions를 deploy에 복사
    - deploy.zip을 S3에 업로드
    - AWS Elastic Beanstalk으로 배포

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 
-// 정적 파일 제공을 위한 경로 설정
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// 모든 경로를 index.html로 라우팅
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
