a4007e1f8a0c3277b98cf4c7a4f40137a77a780e
Bodrami <dlwldms1523@gmail.com>
2024-05-31 09:45:21 +0900

    refactor: #53 #77 Refactoring app.vue And footer
    - main 규격 css로 고정
    - footer 코드 리펙토링 (하단 고정)

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 <!--    <DriverHeader/>-->
   </header>
 
-  <main>
+  <main class="main">
   <router-view/>
   </main>
 
@@ -15,15 +15,25 @@
 <!--  </aside>-->
 
   <footer>
+    <Footer/>
   </footer>
 
 </template>
 
 <script setup>
 import Footer from "@/components/layouts/footer/Footer.vue";
-import RootAdmin from "@/components/layouts/header/RootAdmin.vue";
-import Sidebar from "@/components/layouts/sidebar/sidebar.vue";
+import RootAdmin from "@/components/layouts/header/RootAdminHeader.vue";
+import Sidebar from "@/components/layouts/sidebar/SideBar.vue";
 import DriverHeader from "@/components/layouts/header/DriverHeader.vue";
 import FranchiseHeader from "@/components/layouts/header/FranchiseHeader.vue";
 import AdminHeader from "@/components/layouts/header/AdminHeader.vue";
 </script>
+
+<style scoped>
+.main {
+  width: 1900px;
+  position: relative;
+  left: 110px;
+  height: 1200px;
+}
+</style>
