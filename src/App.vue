b6075e4911ca9b2af9da88e90a51117974946744
악덕 <rudejrqkr97@gmail.com>
2024-06-03 03:08:00 +0900

    fix: Correct user role check in App.vue
    - App.vue에서 사용자 역할 확인 수정
    - 사용자 역할에 따른 헤더 컴포넌트 수정
    - 사용자 역할을 정확히 확인하여 헤더 표시
    - 사용자 역할에 따라 헤더 컴포넌트 변경
    - 사용자 역할에 따라 헤더 표시 여부 수정

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
 <template>
-  <header>
-    <RootAdmin/>
-<!--    <AdminHeader/>-->
-<!--    <FranchiseHeader/>-->
-<!--    <DriverHeader/>-->
-  </header>
+  <div id="app">
+    <header>
+      <RootAdminHeader v-if="userRole === 'ROLE_ROOT'" />
+      <AdminHeader v-else-if="userRole === 'ROLE_ADMIN'" />
+      <FranchiseHeader v-else-if="userRole === 'ROLE_OWNER'" />
+      <DriverHeader v-else-if="userRole === 'ROLE_DRIVER'" />
+    </header>
 
-  <main class="main">
-  <router-view/>
-  </main>
-
-  <aside>
-    <!-- <Sidebar/> -->
-  </aside>
-
-    <main>
+    <main class="main">
       <router-view />
     </main>
-  </div>
-  <footer>
-    <Footer/>
-  </footer>
 
+    <aside>
+      <!-- <Sidebar /> -->
+    </aside>
+
+    <footer>
+      <Footer />
+    </footer>
+  </div>
 </template>
 
 <script setup>
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
 import Footer from "@/components/layouts/footer/Footer.vue";
-import RootAdmin from "@/components/layouts/header/RootAdminHeader.vue";
-import Sidebar from "@/components/layouts/sidebar/SideBar.vue";
+import RootAdminHeader from "@/components/layouts/header/RootAdminHeader.vue";
 import DriverHeader from "@/components/layouts/header/DriverHeader.vue";
 import FranchiseHeader from "@/components/layouts/header/FranchiseHeader.vue";
 import AdminHeader from "@/components/layouts/header/AdminHeader.vue";
 
-
-import { onMounted } from 'vue';
-import {useStore} from 'vuex';
-
 const store = useStore();
+const userRole = computed(() => store.getters.userRole);
 
 onMounted(async () => {
   await store.dispatch('initializeAuth');
 
-  // URL에서 토큰이 있는 경우 로그인 처리
   const urlParams = new URLSearchParams(window.location.search);
   const token = urlParams.get('token');
 
@@ -50,14 +44,10 @@ onMounted(async () => {
     await store.dispatch('initializeAuth');
   }
 });
-
 </script>
 
 <style scoped>
 .main {
-  width: 1900px;
-  position: relative;
-  left: 110px;
-  height: 1200px;
+  margin-top: 2%;
 }
 </style>
