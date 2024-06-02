e2ad223794610f7a9f6193bb9ad4a2e7c3a83cbf
letsplaycoding <sdh989800@gmail.com>
2024-06-02 12:15:42 +0900

    feat: #10 view product
    상품 조회창 option 수정

--- src/components/amdin/product/ProductDetailPopup.vue
+++ src/components/amdin/product/ProductDetailPopup.vue
@@ -15,19 +15,19 @@
                 </td>
                 <td class="second-insert-input">
                   <select v-model="updateFirst" @change="fetchCategories('second')" class="categories">
-                    <option value="">{{currentCategoryFirstName}}</option>
+                    <option value="">{{currentCategoryFirstCode}}</option>
                     <option v-for="category in firstCategories" :key="category.categoryFirstCode" :value="category.categoryFirstCode">
                       {{ category.categoryFirstName }}
                     </option>
                   </select>
                   <select v-model="updateSecond" @change="fetchCategories('third')" class="categories-g">
-                    <option value="">{{currentCategorySecondName}}</option>
+                    <option value="">{{currentCategorySecondCode}}</option>
                     <option v-for="category in secondCategories" :key="category.categorySecondCode" :value="category.categorySecondCode">
                       {{ category.categorySecondName }}
                     </option>
                   </select>
                   <select v-model="updateThird" class="categories-g">
-                    <option value="">{{currentCategoryThirdName}}</option>
+                    <option value="">{{currentCategoryThirdCode}}</option>
                     <option v-for="category in thirdCategories" :key="category.categoryThirdCode" :value="category.categoryThirdCode">
                       {{ category.categoryThirdName }}
                     </option>
@@ -125,9 +125,6 @@ const updateFirst = ref('');
 const updateSecond = ref('');
 const updateThird = ref('');
 const updateContent = ref('');
-const currentCategoryFirstName = ref('');
-const currentCategorySecondName = ref('');
-const currentCategoryThirdName = ref('');
 
 const props = defineProps({
   currentProductCode: String,
@@ -224,38 +221,7 @@ const fetchCategories = async (level) => {
     console.error('Error:', error);
   }
 };
-const getCategoryName = async (categoryCode, level) => {
-  let url = '';
-  switch (level) {
-    case 'first':
-      url = `http://localhost:5000/admin/category/first/${categoryCode}`;
-      break;
-    case 'second':
-      url = `http://localhost:5000/admin/category/second/${categoryCode}`;
-      break;
-    case 'third':
-      url = `http://localhost:5000/admin/category/third/${categoryCode}`;
-      break;
-  }
 
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-      },
-    });
-    if (!response.ok) {
-      throw new Error(`${level} 카테고리 이름을 불러오는 데 실패했습니다.`);
-    }
-    const data = await response.json();
-    return data.categoryName;
-  } catch (error) {
-    console.error('Error:', error);
-    return '';
-  }
-};
 onMounted(async () => {
   const numberInputs = document.querySelectorAll('input[type="number"]');
   numberInputs.forEach(input => {
@@ -268,9 +234,6 @@ onMounted(async () => {
       input.value = input.value.replace(/[^0-9]/g, '');
     });
   });
-  currentCategoryFirstName.value = await getCategoryName(props.currentCategoryFirstCode, 'first');
-  currentCategorySecondName.value = await getCategoryName(props.currentCategorySecondCode, 'second');
-  currentCategoryThirdName.value = await getCategoryName(props.currentCategoryThirdCode, 'third');
 
   await fetchCategories('first');
 });
