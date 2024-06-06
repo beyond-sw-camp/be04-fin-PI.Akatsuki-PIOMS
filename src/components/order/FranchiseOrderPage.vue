c625ceaa63b857d76168cdb3c6eecdcf566b5be7
daersh <daersh@naver.com>
2024-06-06 15:46:32 +0900

    fix: delete unnecessary filter component
    - 불필요한 필터 제거

--- src/components/order/FranchiseOrderPage.vue
+++ src/components/order/FranchiseOrderPage.vue
@@ -20,24 +20,14 @@
           </td>
         </tr>
         <tr>
-          <td class="filter-label">가맹점명</td>
-          <td class="filter-input">
-            <input type="text" v-model="filterFranchiseName" />
-          </td>
-          <td class="filter-label">배송(송장)번호</td>
-          <td class="filter-input">
-            <input type="text" v-model="filterInvoiceCode" />
-          </td>
-        </tr>
-
-        <tr>
-          <td class="filter-label">점주명</td>
-          <td class="filter-input">
-            <input type="text" v-model="filterFranchiseOwnerName" />
-          </td>
           <td class="filter-label">주문(발주)일</td>
           <td class="filter-input">
             <input type="date" v-model="filterOrderDate" />
+          </td>
+          
+          <td class="filter-label">배송(송장)번호</td>
+          <td class="filter-input">
+            <input type="text" v-model="filterInvoiceCode" />
           </td>
         </tr>
 
