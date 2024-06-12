07465e2d91ba222a1bdfb471547ba773da40d629
악덕 <rudejrqkr97@gmail.com>
2024-06-13 03:14:53 +0900

    design: Correct the breadcrumb text in FranchiseOwnerList.vue
    - 가맹점 및 직원 관리 > 가맹점 및 점주 관리 > 점주 관리 수정.

--- src/components/franchise/member/FranchiseOwnerList.vue
+++ src/components/franchise/member/FranchiseOwnerList.vue
@@ -4,7 +4,7 @@
       <div style="  max-width: 1440px;justify-content: center;align-items: center;"  >
         <br>
         <div style="float: left" ><img src="@/assets/icon/smile.png" style="width: 18px"/>&nbsp;
-          <span class="breadcrumb">가맹점 및 직원 관리 > 가맹점 및 점주 관리 > 가맹점 관리</span>
+          <span class="breadcrumb">가맹점 및 직원 관리 > 가맹점 및 점주 관리 > 점주 관리</span>
         </div>
       </div>
     </div>
