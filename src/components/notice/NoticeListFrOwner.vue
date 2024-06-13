63dd112f65d8eea16de9e0fcc4f6f0f2e4895be3
악덕 <rudejrqkr97@gmail.com>
2024-06-13 12:04:51 +0900

    design: Modify some CSS styles in NoticeListFrOwner.vue
    - 컨테이너의 패딩을 20px으로 조정
    - 헤더의 최대 너비를 1440px로 변경
    - 제목 아이콘의 너비 18px로 조정
    - 공지사항 목록의 테이블 너비를 100%로 조정
    - 테이블 셀의 패딩을 10px로 설정
    - 마지막 테이블 셀의 하단 테두리 삭제
    - 테이블 컨테이너의 마진을 20px으로 조정
    - 테이블의 그림자와 간격을 수정하여 레이아웃을 개선함.

--- src/components/notice/NoticeListFrOwner.vue
+++ src/components/notice/NoticeListFrOwner.vue
@@ -1,66 +1,65 @@
 <template>
-  <div class = "main">
-    <div align="center"  style="padding-bottom: 30px;">
-      <div style="  max-width: 1250px;justify-content: center;align-items: center;"  >
+  <div class="container">
+    <div class="header" align="center" style="padding-bottom: 30px;">
+      <div style="  max-width: 1440px;justify-content: center;align-items: center;">
         <br>
-        <div style="float: left" ><img src="@/assets/icon/smile.png" style="width: 18px"/>&nbsp;
-          <span class="breadcrumb">공지 및 문의 관리 > 공지 관리 > 공지사항 조회 및 관리</span>
+        <div style="float: left"><img src="@/assets/icon/공지사항.png" style="width: 18px"/>&nbsp;
+          <span class="breadcrumb">공지 및 문의 관리 &gt; 공지사항 &gt; 공지사항 목록</span>
         </div>
       </div>
     </div>
-    <br/>
-\
-    <div align="center">
-      <table class="read-filter">
+
+    <div class="product-sub-title"> * 조회할 상품의 조건을 선택 후
+      <img src="@/assets/icon/reset.png">초기화 또는<img src="@/assets/icon/search.png">검색을 눌러주세요.
+    </div>
+
+
+    <div class="filter-section">
+      <table class="filter-table">
         <tr>
           <td class="filter-label">공지 조회 조건</td>
-          <td class="filter1">
-            <select class="filter-section" v-model="filterType">
+          <td class="filter-input">
+            <select class="filter-section" v-model="filterType" style="width: 100px">
+              <option disabled value="">선택</option>
               <option value="all">전체</option>
               <option value="content">내용</option>
               <option value="title">제목</option>
             </select>
-
-            <input type="text" class="filter-input" placeholder="검색어를 입력해주세요." v-model="filterText" />
+            <input type="text" class="filter-input" placeholder="검색어를 입력해주세요." v-model="filterText"/>
           </td>
         </tr>
         <tr>
           <td class="filter-label">등록일</td>
-          <td colspan="3" class="filter-date">
-            <input type="date" id="startDate" v-model="startDate" placeholder="시작 날짜 선택" title="시작 날짜 선택" />
+          <td colspan="3" class="filter-input">
+            <input type="date" id="startDate" v-model="startDate" placeholder="시작 날짜 선택" title="시작 날짜 선택"/>
             &nbsp;&nbsp;<span>~</span>&nbsp;
-            <input type="date" id="endDate" v-model="endDate" placeholder="종료 날짜 선택" title="종료 날짜 선택" />
+            <input type="date" id="endDate" v-model="endDate" placeholder="종료 날짜 선택" title="종료 날짜 선택"/>
           </td>
         </tr>
       </table>
+      <div class="filter-buttons">
+        <button @click="resetFilters" class="reset-btn">
+          <img src="@/assets/icon/reset.png" alt="Reset"/>
+        </button>
+        <button @click="applyFilters" class="search-btn">
+          <img src="@/assets/icon/search.png" alt="Search"/>
+        </button>
+      </div>
     </div>
-
-    <!-- 리셋, 검색 버튼 -->
-    <div class="button-container">
-      <button type="button" class="btn-reset" @click="resetFilters">
-        <img class="reset" src="@/assets/icon/reset.png" />
-      </button>
-      <button type="button" class="btn-search" @click="applyFilters">
-        <img class="search" src="@/assets/icon/search.png" />
-      </button>
-    </div>
-  </div>
-
-    <!-- 공지사항 목록 -->
     <div class="table-container">
       <table class="table">
         <thead>
         <tr class="header1">
-          <td>No</td>
-          <td>제목</td>
-          <td>내용</td>
+          <td width="4%">No</td>
+          <td width="20%">제목</td>
+          <td width="55%">내용</td>
           <td>등록일</td>
         </tr>
         </thead>
         <tbody>
         <tr v-for="(item, index) in paginatedLists" :key="item.noticeCode"
             :id="'row-' + rowIndex"
-            class="allpost"
+            class="tr__bd"
         >
           <td class="num">{{ index + 1 + (currentPage - 1) * itemsPerPage }}</td>
           <td style="width: 20%" @click="showDetailsNoticePopup(item)">{{ item.noticeTitle }}</td>
@@ -71,8 +70,9 @@
       </table>
     </div>
 
+
     <!-- 공지사항 상세 정보 팝업 -->
-    <NoticeDetailsPopup v-if="viewPopup && selectedNotice" :notice="selectedNotice" @close="closeDetailsPopup" />
+    <NoticeDetailsPopup v-if="viewPopup && selectedNotice" :notice="selectedNotice" @close="closeDetailsPopup"/>
 
     <!-- 페이지네이션 -->
     <div class="pagination">
@@ -80,13 +80,13 @@
       <span>{{ currentPage }} / {{ totalPages }}</span>
       <button @click="nextPage" :disabled="currentPage === totalPages">다음</button>
     </div>
-
+  </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import {ref, computed, onMounted} from 'vue';
 import NoticeDetailsPopup from '@/components/notice/NoticeDetailsPopup.vue';
-import { useStore } from 'vuex';
+import {useStore} from 'vuex';
 
 const highlightRow = (index) => {
   document.querySelector(`#row-${index}`).classList.add('highlighted');
@@ -100,10 +100,10 @@ const accessToken = store.state.accessToken;
 
 const lists = ref([]);
 const headers = ref([
-  { label: 'No' },
-  { key: 'noticeTitle', label: '공지사항 제목' },
-  { key: 'noticeContent', label: '공지사항 내용' },
-  { key: 'noticeEnrollDate', label: '등록일' }
+  {label: 'No'},
+  {key: 'noticeTitle', label: '공지사항 제목'},
+  {key: 'noticeContent', label: '공지사항 내용'},
+  {key: 'noticeEnrollDate', label: '등록일'}
 ]);
 
 const filteredLists = ref([]);
@@ -137,7 +137,7 @@ const getNotice = async () => {
   }
 };
 
-      // lists.value = noticeList.sort((a, b) => new Date(b.noticeEnrollDate) - new Date(a.noticeEnrollDate));
+// lists.value = noticeList.sort((a, b) => new Date(b.noticeEnrollDate) - new Date(a.noticeEnrollDate));
 const filterType = ref('');
 const filterText = ref('');
 const startDate = ref('');
@@ -207,295 +207,216 @@ onMounted(() => {
 </script>
 
 <style scoped>
-@import "../../assets/css/order.css" ;
-
-.modify,
-.delete {
-  color: #ffffff;
-  font-weight: bold;
-  border-radius: 5px;
-  border: 0;
-  width: 50px;
-}
-
-.modify {
-  background-color: #999999;
-}
-.delete {
-  background-color: #FC6F86;
-}
-.table-td button {
-  border: none;
-  background-color: #ffffff;
-}
-/* 공통 스타일 */
 .container {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.header {
+  width: 100%;
+  max-width: 1440px;
+  text-align: center;
+  padding-bottom: 30px;
+}
+
+.header-content {
   display: flex;
   align-items: center;
-  justify-content: flex-start;
-  max-width: 800px;
-  margin-left: 5px;
-  padding: 20px;
-  position: relative;
-  top: 10px;
 }
 
-.F-title {
-  margin-right: 10px;
+.header-icon {
+  width: 18px;
+}
+
+.breadcrumb {
+  font-size: 16px;
+  color: #555;
   font-weight: bold;
-  font-size: 20px;
-  position: relative;
-  top: 2px;
 }
 
-.notice {
-  margin-right: 10px;
-}
-
-.read-filter {
+.product-sub-title {
+  display: flex;
+  align-items: center;
+  gap: 5px;
   margin-bottom: 20px;
-  background-color: #ffffff;
-  border: 1px solid #D9D9D9;
+  justify-content: flex-start;
+  width: 100%;
+  max-width: 1440px;
+  padding-left: 10px;
+}
+
+.filter-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+  width: 100%;
+  max-width: 1440px;
+}
+
+.filter-table {
+  border-collapse: collapse;
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
   border-radius: 5px;
   padding: 10px;
-  width: 1250px;
+  width: 100%;
 }
 
-.read-filter td {
-  border: none;
-}
-
-.filter-label,
-.filter1,
-.filter-date {
-  cursor: default;
+.filter-table td {
+  padding: 5px 10px;
 }
 
 .filter-label {
-  font-size: 16px;
-  text-align: center;
-  width: 120px;
-  height: 60px;
   font-weight: bold;
-  background-color: #D9D9D9;
-  color: #444444;
-}
-
-.filter-section {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100px;
-  height: 52px;
-  font-size: 14px;
-  margin-bottom: 0;
-  border-color: #d9d9d9;
+  text-align: center;
+  border: solid 1px #747474;
+  width: 120px;
+  background-color: #d9d9d9;
 }
 
 .filter-input {
-  width: 400px;
-  height: 30px;
-  padding: 10px;
-  margin-left: 20px;
-  border-right: 1px solid #d9d9d9;
+  text-align: left;
+  border: solid 1px #747474;
+  padding: 5px;
 }
 
-.filter1 {
-  font-size: 14px;
+.filter-buttons {
   display: flex;
-  justify-content: flex-start;
-  margin-left: 10px;
-  position: relative;
-  top: 4px
+  justify-content: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+  max-width: 1440px;
 }
 
-.filter-input,
-.filter-section {
-  font-size: 16px;
+.reset-btn,
+.search-btn {
+  background-color: #fff;
+  color: black;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  padding: 8px 16px;
+  font-size: 14px;
+  margin: 0 5px;
 }
 
-.filter-date {
-  border-top: 1px solid #D9D9D9 !important;
-  border-color: #d9d9d9;
+.table th {
+  font-weight: bold;
+  color: #000;
+  text-align: center;
+}
+
+.table th,
+.table td {
+  padding: 10px;
+  text-align: center;
+}
+
+.header1 {
+  background-color: #d9d9d9;
+  font-weight: bold;
+  height: 50px;
+  font-size: 14px;
+  text-align: center;
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  margin-bottom: 100px;
+}
+
+.pagination button {
+  background-color: #fff;
+  color: black;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  cursor: pointer;
+  padding: 8px 16px;
+  font-size: 14px;
+  margin: 0 5px;
+}
+
+.pagination button:disabled {
+  cursor: not-allowed;
+  opacity: 0.5;
+}
+
+.pagination span {
+  margin: 0 10px;
+  font-weight: bold;
+}
+
+.post-btn {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1440px;
+}
+
+.postBtn {
+  border: none;
+  background-color: white;
+  cursor: pointer;
+}
+
+.excelBtn {
+  border: none;
+  background-color: white;
+  cursor: pointer;
+}
+
+.tr__bd:hover {
+  background-color: #f2f2f2;
+}
+
+.tr__bd td {
+  border-bottom: 1px solid #ddd;
+  font-size: 12px;
+}
+
+.tr__bd:last-child td {
+  border-bottom: none;
 }
 
 .table-container {
   width: 100%;
-  margin-bottom: 10px;
-
+  margin-bottom: 20px;
   display: flex;
   justify-content: center;
 }
 
-.table{
-  width: 1250px;
-  max-width: 1300px;
+.table {
+  width: 100%;
+  max-width: 1440px;
   border-collapse: collapse;
   background-color: #fff;
   border-radius: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   border-spacing: 0 10px;
+  table-layout: fixed; /* 고정된 레이아웃을 사용하여 셀 너비를 고정 */
 }
 
-
-
-.notice-list {
-  width: 1600px;
-}
-.notice-list tr td {
+.table th, .table td {
+  padding: 10px;
   text-align: center;
+  white-space: nowrap; /* 텍스트 줄바꿈 방지 */
+  overflow: hidden;
+  text-overflow: ellipsis; /* 내용이 넘칠 때 "..." 표시 */
 }
-.notice-list thead tr td {
-  background-color: #d9d9d9;
+.condition-button {
+  display: inline-block;
+  padding: 2px 5px; /* 최소한의 패딩으로 글씨 영역만큼만 색이 적용되도록 합니다 */
+  border-radius: 3px;
+  color: #fff;
   font-weight: bold;
 }
 
-.notice-table th,
-.notice-table td {
-  padding: 10px;
-  font-size: 14px;
-  color: #444444;
-}
-
-.notice-table th {
-  background-color: #d9d9d9;
-}
-
-.notice-table td {
-  position: relative;
-  top:5px;
-}
-
-/* 팝업 */
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-notice-form {
-  background: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  max-width: 700px; /* 팝업의 최대 너비 */
-}
-
-.notice-form h2 {
-  margin-bottom: 16px;
-}
-
-.notice-form form div {
-  margin-bottom: 12px;
-}
-
-.notice-form input,
-.notice-form textarea {
-  width: calc(100% - 20px); /* 팝업의 패딩 값 제외한 너비 */
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-
-.notice-form button {
-  padding: 8px 12px;
-  margin-right: 8px;
-  border: none;
-  background: #007bff;
-  color: white;
-  cursor: pointer;
-  border-radius: 4px;
-}
-
-.notice-form button[type="button"] {
-  background: #6c757d;
-}
-
-.notice-form button:disabled {
-  background: #ccc;
-  cursor: not-allowed;
-}
-
-#startDate,
-#endDate {
-  width: 170px;
-  height: 30px;
-  font-size: 16px;
-  padding: 10px;
-  margin-left: 10px;
-  border: 1px solid #d9d9d9;
-  color: #444444;
-}
-
-.button-container {
-  display: flex;
-  justify-content: center; /* 버튼을 왼쪽에 정렬 */
-  align-items: center; /* 버튼을 수직 가운데에 정렬 */
-  gap: 10px; /* 버튼 사이의 간격을 설정 */
-}
-.notice-btn-container{
-  display: flex;
-  justify-content: center; /* 버튼을 왼쪽에 정렬 */
-  align-items: center; /* 버튼을 수직 가운데에 정렬 */
-}
-
-.btn-saveNotice {
-
-  display: flex;
-  position: relative;
-  //left: 10px;
-  height: 35px;
-  font-size: 16px;
-  font-weight: bold;
-  border-radius: 5px;
-  border-color: #344DAF;
-  background-color: #344DAF;
-  color: #ffffff;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  border: 0;
-  margin-bottom: 10px;
-  width: 115px;
-
-}
-
-.btn-saveNotice:hover {
-  background-color: #9a9a9a;
-  color: #ffffff;
-}
-
-.btn-reset,
-.btn-search {
-  border: none;
-  background-color: #ffffff;
-}
-
-.reset,
-.search {
-  width: 40px;
-  height: 40px;
-  justify-content: center;
-  border: none;
-  background-color: #ffffff;
-}
-
-.headerTitle img {
-  width: 10px;
-  height: 10px;
-}
-.headerTitle p {
-  font-size: 20px;
-  font-weight: bold;
-}
-
-.pagination{
-  padding-bottom: 150px;
-}
 </style>
